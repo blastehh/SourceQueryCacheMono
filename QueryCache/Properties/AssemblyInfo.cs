9d322aa6f66283dc501489a9a3ee468f2a5c1961
Matthew Chan <blastuk@gmail.com>
2015-08-06 21:17:31 +0100

    Author update

--- QueryCache/Properties/AssemblyInfo.cs
+++ QueryCache/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("m00")]
+[assembly: AssemblyCopyright ("Blasteh")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
